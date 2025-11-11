665387c453cdad9e3f9edd7b322d5b6043a1bb34
javier <ja.quirogab@duocuc.cl>
2025-11-11 13:50:37 -0300

    commit2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 	kotlin("plugin.jpa") version "1.9.25"
 }
 
-group = "com.proyecto"
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
-description = "proyecto-backend"
+description = "Demo project for Spring Boot"
 
 java {
 	toolchain {
@@ -22,11 +22,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.oracle.database.jdbc:ojdbc11")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/proyecto/backend/controller/ProductoController.kt
+++ src/main/kotlin/com/proyecto/backend/controller/ProductoController.kt
@@ -1,4 +1,38 @@
 package com.proyecto.backend.controller
 
-class ProductoController {
+import com.proyecto.backend.model.Producto
+import com.proyecto.backend.service.ProductoService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/productos")
+class ProductoController(
+    private val productoService: ProductoService
+){
+    @GetMapping
+    fun listar(): List<Producto> = productoService.listar()
+
+    @PostMapping
+    fun crear(@RequestBody producto: Producto):Producto = productoService.crear(producto)
+
+    @GetMapping("/{id}")
+    fun obtenerPorId(@PathVariable id:Long):Producto = productoService.obtenerPorId(id)
+
+    @PutMapping("/{id}")
+    fun actualizar(@PathVariable id:Long, @RequestBody datos:Producto):Producto{
+        val producto = productoService.obtenerPorId(id)
+        val actualizado = producto.copy(idProducto = datos.idProducto, nomnbreProducto = datos.nomnbreProducto, precioProducto = datos.precioProducto)
+        return productoService.crear(actualizado)}
+
+    @DeleteMapping("/{id}")
+    fun eliminar (@PathVariable id:Long){
+        productoService.eliminar(id)
+    }
 }
